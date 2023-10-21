03e2bf1b268065e6b878aef5a333cada6514bc12
FakirHerif <basdemir35@hotmail.com.tr>
2023-10-21 19:01:00 +0300

    added input model

--- capstone/models.py
+++ capstone/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Input(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.CharField(max_length=6000)
+    url = models.CharField(max_length=60)
+    sites = models.CharField(max_length=30)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
+    
