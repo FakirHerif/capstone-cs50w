b1c3375b7ab15387391bd64e6a15954d0c4a66e4
FakirHerif <basdemir35@hotmail.com.tr>
2023-10-22 00:49:52 +0300

    the problem of inactive input items appearing in the bookmarks section has been resolved, and input details are now visible in the admin panel

--- capstone/admin.py
+++ capstone/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from .models import User,Category,Input,Comment
 
 # Register your models here.
+
+
+class InputAdmin(admin.ModelAdmin):
+    list_display = ('title', 'isActive', 'owner', 'category')
+    list_filter = ('isActive', 'category')
+
+
 admin.site.register(User)
 admin.site.register(Category)
-admin.site.register(Input)
+admin.site.register(Input, InputAdmin)
 admin.site.register(Comment)
