5893a66f654a6e716622728e7733e69ad712ad96
FakirHerif <basdemir35@hotmail.com.tr>
2023-10-23 01:41:56 +0300

    site selection set to devdocs by default and left tab added in input.html

--- capstone/models.py
+++ capstone/models.py
@@ -13,10 +13,19 @@ class Category(models.Model):
         return self.categoryName
 
 class Input(models.Model):
+
+    SITE_CHOICES = [
+        ('DevDocs', 'DevDocs'),
+    ]
+
+
     title = models.CharField(max_length=30)
     content = models.CharField(max_length=6000)
     url = models.CharField(max_length=60)
-    sites = models.CharField(max_length=30)
+    sites = models.CharField(max_length=30,
+        choices=SITE_CHOICES,
+        default='DevDocs',
+    )
     owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
     isActive = models.BooleanField(default=True)
