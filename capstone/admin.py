a09b7d933273826692a50a10a4ec2f14e037cff6
FakirHerif <basdemir35@hotmail.com.tr>
2023-10-23 06:31:53 +0300

    admin panel has been customized, search and filter features have been added

--- capstone/admin.py
+++ capstone/admin.py
@@ -1,25 +1,35 @@
 from django.contrib import admin
 from .models import User,Category,Input,Comment,Site
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'groups',  'inputBookmark')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
 
 
 class InputAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'isActive', 'owner', 'category', 'content')
-    list_filter = ('isActive', 'category')
+    list_filter = ('isActive', 'category', 'owner')
+    search_fields = ('title', 'content', 'owner__username', 'category__categoryName')
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('categoryName', 'categoryImage')
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('id', 'author', 'input', 'category', 'message')
-    list_filter = ('category',)
+    list_filter = ('category', 'author', 'input')
+    search_fields = ('author__username', 'input__title', 'category__categoryName', 'message')
 
 class SiteAdmin(admin.ModelAdmin):
     list_display = ('id', 'author', 'name', 'url', 'input', 'category')
-    list_filter = ('category', 'name',)
-
-admin.site.register(User)
+    list_filter = ('category', 'name', 'input', 'author')
+    search_fields = ('author__username', 'input__title', 'category__categoryName', 'name', 'url')
+    
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Input, InputAdmin)
 admin.site.register(Comment, CommentAdmin)
