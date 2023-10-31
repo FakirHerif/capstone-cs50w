2c246c3f5f1b8efd36d4b165046aaba59f261786
FakirHerif <basdemir35@hotmail.com.tr>
2023-11-01 00:42:38 +0300

    scroll up/scroll down elements have been added to the layout page so that they can be used throughout the site

--- capstone/static/capstone/script.js
+++ capstone/static/capstone/script.js
@@ -1,1 +1,24 @@
 console.log("LOADED!")
+
+document.addEventListener("DOMContentLoaded", function() {
+const scrollTopButton = document.getElementById("scrollTopButton");
+
+scrollTopButton.addEventListener("click", function() {
+    event.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+const scrollBottomButton = document.getElementById("scrollBottomButton");
+
+
+scrollBottomButton.addEventListener("click", () => {
+    event.preventDefault();
+    window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: "smooth"
+  });
+});
+});
